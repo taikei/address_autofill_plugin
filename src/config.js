73aae783ab30a65cfc380d2031168a396a454250
Taikei Nakano <taikei.nakano@sazae.com.au>
2021-07-11 21:50:36 +1000

    refined config.js

--- src/config.js
+++ src/config.js
@@ -7,28 +7,13 @@ jQuery.noConflict();
 
   let $form = $('.js-submit-settings');
   let $cancelButton = $('.js-cancel-button');
-
-  // let $postcodeField = $('#config-select-postcode');
-  // let $addressField = $('#config-select-address');
-  // let $suburbField = $('#config-select-suburb');
-  // let $stateField = $('#config-select-state');
-  let fieldArr =[];
-
-
-  let $postcodeField = $('#postcode');
-  let $addressField = $('#address');
-  let $suburbField = $('#suburb');
-  let $stateField = $('#state');
-
-
-  const configFields = $('.kintoneplugin-row');
-  console.log(configFields);
+  const $configFields = $('.kintoneplugin-row');
 
   let dropdown = new Dropdown({
     items:[],
     value: ``
   });
-
+  let dropdownCopy;
   
   // let dropdown = new Dropdown({
   //   items: [
@@ -46,46 +31,34 @@ jQuery.noConflict();
 
   
 
-   // 前回の設定情報を反映 (Bring previous configuration to the page)
-   $(document).ready(()=>{
+  // 前回の設定情報を反映 (Bring previous configuration to the page)
+  $(document).ready(()=>{
     let conf = kintone.plugin.app.getConfig(PLUGIN_ID);
-    //console.log(conf);
-  
+    console.log(conf);
+
     // $test.append(dropdown.render());
   })
 
+
+  
   //　フィールドの一覧を取得 (fetch fields list)
   kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', {'app': kintone.app.getId()}, function(resp) {
-    // success
+    // success　取得に成功した場合
+
     let fieldProperties = resp['properties'];
-    console.log(fieldProperties);
 
-    for (let i = 0; i < configFields.length; i ++){
-      console.log(configFields[i]);
-
+    for (let i = 0; i < $configFields.length; i ++){
+      
       jQuery.each(fieldProperties, (key, value) => {
 
-        
-        const fieldName = value.label;
-        const fieldCode = value.code;
-
         dropdown.addItem({
-          label: fieldName,
-          value: fieldCode
+          label: value.label,
+          value: value.code
         })
-        
-  
-        configFields[i].append(dropdown.render());
-        //  $postcodeField.append(`<option id="${value.code}">${value.label}</option>`);
-        //  $addressField.append(`<option id="${value.code}">${value.label}</option>`);
-        //  $suburbField.append(`<option id="${value.code}">${value.label}</option>`);
-        //  $stateField.append(`<option id="${value.code}">${value.label}</option>`);
-        //  fieldArr.push({name: value.label, code: value.code});
-
-        
       })
 
-      console.log()
+      dropdownCopy = JSON.parse(JSON.stringify(dropdown.itemComps));
+      $configFields[i].append(dropdown.render());
 
       dropdown = new Dropdown({
         items:[],
@@ -93,32 +66,29 @@ jQuery.noConflict();
       });
 
     }
+    
   }, function(error) {
     // error
     console.log(error);
   });
 
 
-  
+  // 'Saveボタン押下で setConfig()'
   $form.submit( e => {
     e.preventDefault();
 
+    let $valueFields = $('.kuc-dropdown-selected-label');
     let config = {}
-
-    fieldArr.forEach((val, ind) => {
-      if(val.name === $postcodeField.val()){
-        config.postcode = {code: val.code};
+    
+    for (let i = 0; i < $valueFields.length; i++){
+      let selectedValue = $valueFields[i].innerText;
+      for (let j = 0; j < dropdownCopy.length; j++){
+        if(dropdownCopy[j]._props.item.label == selectedValue){
+          config[`${selectedValue}`] = dropdownCopy[j]._props.item.value;
+        }
       } 
-      if(val.name === $addressField.val()){
-        config.address = val.code;
-      } 
-      if(val.name === $suburbField.val()){
-        config.suburb = val.code;
-      } 
-      if(val.name === $stateField.val()){
-        config.state = val.code;
-      } 
-    })
+    }
+    console.log(config);
 
     kintone.plugin.app.setConfig(config, () => {
       window.location.href = '../../flow?app=' + kintone.app.getId();
@@ -126,7 +96,10 @@ jQuery.noConflict();
 
   });
 
+  
   $cancelButton.on('click', function () {
     window.location.href = '../../' + kintone.app.getId() + '/plugin/';
   });
+
+
 })(jQuery, kintone.$PLUGIN_ID);
